bd8c9ae6767863cf81b9de433a9e4ed2afd897ad
Rakesh <chipparakesh01@gmail.com>
2024-05-19 15:47:15 +0530

    add joblib

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+import joblib
+
+app = Flask(__name__)
+
+clfModel = joblib.load('model.pkl')
+vectorizer = joblib.load('vectorizer.pkl')
+
+
+@app.route("/")
+def predictCat():
+    data = request.get_json()
+    text=data['text']
+    print(len(text))
+    vec = vectorizer.transform(text)
+    pred = clfModel.predict(vec)
+    res=list(pred)
+    return jsonify(res)
+
+if __name__ == '__main__':
+    app.run()
